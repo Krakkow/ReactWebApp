0067231edb4be6bdc770d324ca08f7fcc9e4a1b4
Ilya Shusterman <ilyashusterman@outlook.com>
2016-12-01 01:09:28 +0200

    axios implements login

--- src/main/webapp/App.js
+++ src/main/webapp/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import LoginForm from './src/components/LoginForm';
+import render from 'react-dom';
+import { browserHistory, Router, Route, Link, withRouter } from 'react-router';
+import Header from './src/components/common/Header';
+// import auth from './src/components/common/auth';
 
 class App extends React.Component{
 
@@ -8,6 +12,18 @@ class App extends React.Component{
         this.state = {
             didLoggedIn: false
         }
+    }
+
+    updateAuth(loggedIn) {
+        this.setState({
+            loggedIn
+        })
+    }
+
+
+    componentWillMount() {
+        auth.onChange = this.updateAuth;
+        auth.login()
     }
     renderLoginHome() {
         if(!this.state.didLoggedIn) {
@@ -22,14 +38,15 @@ class App extends React.Component{
         )
     }
 
-    handleLogin() {
-        return;
+    handleLogin(username, password, clientType) {
+        auth.login(username, password, clientType);
+        auth.onChange = this.updateAuth;
     }
     render() {
         return (
             <div>
-                {/*{this.renderLoginHome.bind(this)}*/}
-                <LoginForm loginUser={this.handleLogin} />
+                {this.renderLoginHome.bind(this)}
+                {/*<LoginForm loginUser={this.handleLogin.bind(this)} />*/}
             </div>
         );
     }
--- src/main/webapp/src/components/LoginForm.js
+++ src/main/webapp/src/components/LoginForm.js
@@ -7,15 +7,49 @@ class LoginForm extends React.Component {
         this.state = {
             password: '',
             username:'',
-            clientType: ''
+            clientType: '',
+            error: '',
+            loading: false
         };
     }
 
+    onButtonPress() {
+        const { username, password, clientType } = this.state;
+
+        this.setState({ error: '', loading: true });
+
+        axios.post('/login', {
+            username: username,
+            password: password,
+            clientType: clientType
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    onLoginFail() {
+        this.setState({
+            error: 'Authentication Failed ', loading: false
+        });
+    }
+
+    onLoginSuccess() {
+        this.setState({
+            email: '',
+            password: '',
+            error: '',
+            loading: false
+        });
+    }
     render() {
         return (
-            <form className="form" onSubmit={this._handleSubmit.bind(this)}>
+            <form className="form-control" onSubmit={this.onButtonPress.bind(this)}>
               <label>Login</label>
-              <div className="form-fields">
+              <div>
                 <input type="text" placeholder="username:" ref={c => this.username = c} onChange={this._getCharacterCount.bind(this)} />
                 <p>{this.state.username.length} letters</p>
               </div>
@@ -23,7 +57,7 @@ class LoginForm extends React.Component {
                 <input type="password" placeholder="password:" ref={c => this.password = c} onChange={this._getCharacterCount.bind(this)}/>
                 <p>{this.state.password.length} letters</p>
               </div>
-                <div>
+                <div >
                     <select type="text" placeholder="clientType:" ref={c => this.clientType = c} onChange={this._getCharacterCount.bind(this)}>
                         <option>Admin</option>
                         <option>Company</option>
@@ -31,8 +65,8 @@ class LoginForm extends React.Component {
                     </select>
                     <p>{this.state.clientType.length} letters</p>
                 </div>
-              <div className="comment-form-actions">
-                <button type="submit">
+              <div >
+                <button className="btn btn danger" type="submit">
                   Login Now!
                 </button>
               </div>
@@ -55,7 +89,8 @@ class LoginForm extends React.Component {
             alert('Please enter your username and password.');
             return;
         }
-
+        // this is the method that calls his parent method to operate a user login!
+        // with username password and clientType
         this.props.loginUser(this.username.value, this.password.value, this.clientType.value);
 
         this.username.value = '';
